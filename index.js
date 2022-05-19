8a2e1ffb8456ad69044ae7881bc56b26a56f0474
aco4 <anthonythegu@gmail.com>
2022-05-19 16:18:03 -0400

    add daily quote

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var index
-var old_index
-
 function random_quote() {
   old_index = index
 
@@ -8,6 +5,11 @@ function random_quote() {
     index = Math.floor(Math.random() * quotes.length);
   }
 
+  document.getElementById("quote").innerHTML = output_quote(index);
+  document.getElementById("description").innerHTML = output_description(index);
+}
+
+function output_quote(index) {
   var quote = quotes[index].text
 
   if (quotes[index].author != null) {
@@ -19,15 +21,29 @@ function random_quote() {
   if (quotes[index].date != null) {
     quote = quote + ", " + quotes[index].date;
   }
+}
 
+function output_description(index) {
   description = ""
 
   if (quotes[index].description != null) {
     description = quotes[index].description;
   }
-
-  document.getElementById("quote").innerHTML = quote;
-  document.getElementById("description").innerHTML = description;
 }
 
+function mulberry32(a) {
+  var t = a += 0x6D2B79F5;
+  t = Math.imul(t ^ t >>> 15, t | 1);
+  t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+  return ((t ^ t >>> 14) >>> 0) / 4294967296;
+}
+
+
+var index
+var old_index
+const date = new Date().getDate()
+
 random_quote();
+
+document.getElementById("daily_quote").innerHTML = output_quote(Math.floor(mulberry32(date) * quotes.length));
+document.getElementById("daily_description").innerHTML = output_description(Math.floor(mulberry32(date) * quotes.length));
