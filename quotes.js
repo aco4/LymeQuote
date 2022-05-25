3e12e24b1226ffaec237b620da0bfebf482f8125
aco4 <anthonythegu@gmail.com>
2022-05-25 18:41:52 -0400

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -188,105 +188,105 @@ quotes =
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    {
-        "text": "Can't fly an airplane while looking down",
-        "author": "Logan Bennett",
-        "date": 2022,
-        "description": "Logan gave this quote during FOT"
-    },
-    {
-        "text": "Big fat hairy deal lasagna time funny large orange cat",
-        "author": "Garfield"
-    },
-    {
-        "text": "allah cat allah cat",
-        "author": "allah cat",
-        "date": null,
-        "description": "allah cat"
-    },
-    {
-        "text": "THEY HIT THE PENTAGON",
-        "author": "Zach Hadel",
-        "date": null,
-        "description": "This is from a youtube video"
-    },
-    {
-        "text": "Matt loves skin",
-        "author": "Anthony Gu",
-        "date": 2022,
-        "description": "That's why he joined the group chat, he was attracted by the furby skin"
-    },
-    {
-        "text": "Early bird gets the worm, but I am wolf not lion or tiger, I don't perform in circus",
-        "author": "Rich Richman McRibRicher",
-        "date": 1999,
-    },
-    {
-        "text": "It is only when mosquito land on your balls that you realize there is a way to solve problems without using violence.",
-        "author": "Confucius",
-        "date": null,
-        "description": "Confucius did not actually say this (probably)."
-    },
-    {
-        "text": "I'd rather be hated for who I am than loved for who I am not.",
-        "author":"Kurt Cobain"
-    },
-    {
-        "text": "Squangle beedorp wingle jangle bango bongo dagy your dad is now lady",
-        "author":"Matthew Schulte",
-        "date": 2022,
-        "description": "Haha loser u got trolled ur dad is actually man still quangle dongle i will find your location and fondle"
-    },
-    {
-        "text": "I'm not a senior yet; however,",
-        "author":"Gabe Newell",
-        "date": 2022,
-        "description": "I should have burned this place down while I had the chance"
-    },
-    {
-        "text": "I miss the mask because I could say whatever I wanted to say and nobody could see me say it.",
-        "author": "Mr. Benfield",
-        "date": 2022,
-        "description": "That's what the purpose of the mask is. (This quote is in reference to the lifted mask mandate following the COVID-19 pandemic)"
-    },
-    {
-        "text": "I was happy yesterday because I did not take a picture with crazy Julia for Human Encyclopedia.",
-        "author":"Minh Tran",
-        "date": 2022,
-        "description": "Julia Krazcek does not know anything except for classical music, literature, and diabetes."
-    },
-    {
-        "text": "High school is a mega-pint of boring",
-        "author":"Josh Birch",
-        "date": 2022,
-    },
-    {
-        "text": "Sometimes, you just got to send it.",
-        "author":"Ryan Kohler",
-        "date": 2022,
-    },
-    {
-        "text": "Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.",
-        "author":"Master Oogway",
-        "date": null,
-        "description": "From Kung Fu Panda"
-    },
-    {
-        "text": "Let the world burn",
-        "author":"Eric Huang",
-        "date": 2022
-    },
-    {
-        "text": "I can be your stepmom",
-        "author":"Alayna LeVee",
-        "date": 2022
-    },
-    {
-        "text": "according to the bible lesbian sex isnt a sin",
-        "author":"sugar",
-        "date": 2022,
-        "description": "im an atheist"
-    },
+    // {
+    //     "text": "Can't fly an airplane while looking down",
+    //     "author": "Logan Bennett",
+    //     "date": 2022,
+    //     "description": "Logan gave this quote during FOT"
+    // },
+    // {
+    //     "text": "Big fat hairy deal lasagna time funny large orange cat",
+    //     "author": "Garfield"
+    // },
+    // {
+    //     "text": "allah cat allah cat",
+    //     "author": "allah cat",
+    //     "date": null,
+    //     "description": "allah cat"
+    // },
+    // {
+    //     "text": "THEY HIT THE PENTAGON",
+    //     "author": "Zach Hadel",
+    //     "date": null,
+    //     "description": "This is from a youtube video"
+    // },
+    // {
+    //     "text": "Matt loves skin",
+    //     "author": "Anthony Gu",
+    //     "date": 2022,
+    //     "description": "That's why he joined the group chat, he was attracted by the furby skin"
+    // },
+    // {
+    //     "text": "Early bird gets the worm, but I am wolf not lion or tiger, I don't perform in circus",
+    //     "author": "Rich Richman McRibRicher",
+    //     "date": 1999,
+    // },
+    // {
+    //     "text": "It is only when mosquito land on your balls that you realize there is a way to solve problems without using violence.",
+    //     "author": "Confucius",
+    //     "date": null,
+    //     "description": "Confucius did not actually say this (probably)."
+    // },
+    // {
+    //     "text": "I'd rather be hated for who I am than loved for who I am not.",
+    //     "author":"Kurt Cobain"
+    // },
+    // {
+    //     "text": "Squangle beedorp wingle jangle bango bongo dagy your dad is now lady",
+    //     "author":"Matthew Schulte",
+    //     "date": 2022,
+    //     "description": "Haha loser u got trolled ur dad is actually man still quangle dongle i will find your location and fondle"
+    // },
+    // {
+    //     "text": "I'm not a senior yet; however,",
+    //     "author":"Gabe Newell",
+    //     "date": 2022,
+    //     "description": "I should have burned this place down while I had the chance"
+    // },
+    // {
+    //     "text": "I miss the mask because I could say whatever I wanted to say and nobody could see me say it.",
+    //     "author": "Mr. Benfield",
+    //     "date": 2022,
+    //     "description": "That's what the purpose of the mask is. (This quote is in reference to the lifted mask mandate following the COVID-19 pandemic)"
+    // },
+    // {
+    //     "text": "I was happy yesterday because I did not take a picture with crazy Julia for Human Encyclopedia.",
+    //     "author":"Minh Tran",
+    //     "date": 2022,
+    //     "description": "Julia Krazcek does not know anything except for classical music, literature, and diabetes."
+    // },
+    // {
+    //     "text": "High school is a mega-pint of boring",
+    //     "author":"Josh Birch",
+    //     "date": 2022,
+    // },
+    // {
+    //     "text": "Sometimes, you just got to send it.",
+    //     "author":"Ryan Kohler",
+    //     "date": 2022,
+    // },
+    // {
+    //     "text": "Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.",
+    //     "author":"Master Oogway",
+    //     "date": null,
+    //     "description": "From Kung Fu Panda"
+    // },
+    // {
+    //     "text": "Let the world burn",
+    //     "author":"Eric Huang",
+    //     "date": 2022
+    // },
+    // {
+    //     "text": "I can be your stepmom",
+    //     "author":"Alayna LeVee",
+    //     "date": 2022
+    // },
+    // {
+    //     "text": "according to the bible lesbian sex isnt a sin",
+    //     "author":"sugar",
+    //     "date": 2022,
+    //     "description": "im an atheist"
+    // },
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -294,69 +294,105 @@ quotes =
 
     {
         "text": "Millions of people worldwide suffer from autoimmune disease and chronic illness. What is fueling this 21st-century pandemic?",
-        "author": "Chronic",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "As Dr. Steven Philips, MD reveals in his novel, 'Chronic', there is compelling evidence that Lyme disease is also the source of chronic disorders such as autoimmune conditions."
     },
     {
         "text": "Nothing is more agonizing than feeling inexplicably sick.",
-        "author": "Dr. Steven Philips and Dana Parish",
-        "date": 2020,
+        "author": "Steven Philips and Dana Parish",
+        "date": "2020",
         "description": "This quote was excerpted from a novel titled 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Healthy Again.'"
     },
     {
         "text": "As much as we love nature, it's become a veritable hazard zone throughout the world.",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "Thanks to climate change, disease-carrying bugs have exploded in number and spread to places they could not have previously survived. For example, ticks, 50% of which carry pathogens (Dr. Steven Philips, MD). Ticks are especially dangerous for their likelihood to cause Lyme disease, a complex and poorly understood disease that can then cause autoimmune reactions. To read more about Lyme disease and its link to autoimmune disorders, read Dr. Steven Philips' novel, 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Health Again'."
     },
     {
         "text": "[Jeffrey Wigand] became an informant for the media in the mid-1990s, exposing how Big Tobacco... promoted [cigarettes] knowing they were addictive and carcinogenic.",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "There is compelling evidence that Lyme disease is also the source of chronic disorders such as autoimmune conditions. Dr. Steven Philips, MD and Dana Parish explore this topic in their novel, 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Healthy Again'. One of their main topics is how the medical community shuns and silences Lyme disease and its victims. Why? Because immunosuppresants are expensive — and profitable."
     },
     {
         "text": "Lyme is known as The Great Imitator. But we consider Lyme, or more accurately Lyme+, \"The Great Cause.\"",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "Lyme disease is a bacterial infection commonly transmitted through bug bites. However, Lyme disease is often accompanied by several other diseases — this cocktail of infections is what Dr. Steven Philips calls \"Lyme+.\" But calling these infections \"imitators\" misrepresents the truth: these infections actually cause a very wide range of chronic and autoimmune diseases."
     },
     {
         "text": "The vast majority of people also view Lyme as a problem confined to certain areas of the world, notably the northeastern U.S. Wrong. Wrong.",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": 2020,
         "description": "Lyme disease has been reported in all 50 states except Hawaii. Additionally, Lyme disease is not just a \"tick-borne illness\"; it can also be transmitted via fleas, lice, sand flies, spiders, ants, even cat scratches/bites. Dr. Steven Philips and Dana Parish explore Lyme disease further in their eye-opening novel, 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Healthy Again'"
     },
     {
         "text": "Semantics guide care.",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "This quote was excerpted from a novel titled 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Healthy Again.' This novel reveals many truths about Lyme disease, including the truth about \"Post-Treatment Lyme Disease Syndrome.\" The particular word choice of this phrase suggests that the symptoms after short-term Lyme therapy are not caused by an ongoing, unkilled infection — which is not the truth."
     },
     {
         "text": "No, you're not crazy. You know your body. You know something is not right and you're not getting the right attention. Follow your gut.",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "This quote was excerpted from a novel titled 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Healthy Again.' In this novel, Dr. Steven Philips, MD and Dana Parish provide compelling evidence that chronic Lyme disease exists, even when doctors around the world insist otherwise."
     },
     {
         "text": "Arrogance frequently goes hand in hand with ignorance.",
-        "author": "Dr. Steven Philips and Dana Parish",
+        "author": "Steven Philips and Dana Parish",
         "date": "2020",
         "description": "This quote was excerpted from a novel titled 'Chronic: The Hidden Cause of the Autoimmune Pandemic and How to Get Healthy Again.' In this novel, Dr. Steven Philips, MD and Dana Parish describe how the medical world suffers from the same vices as other areas of life. In this particular case, how many doctors in the medical world refuse to believe that Lyme disease can cause autoimmune disorders such as rheumatoid arthritis."
-    }, 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    },
     {
-        "text": "I did not invite you over to my house for wine, just to throw your wife through my coffee table",
-        "author": "Sam Hyde",
+        "text": "I didn't know who I was until I treated Lyme.",
+        "author": "Dana Parish",
+        "date": "2020",
+        "description": "Lyme disease is a bacterial infection commonly transmitted through bug bites. Doctors are unfamiliar with this disease, and often misdiagnose it as an autoimmune disorder."
+    },
+    {
+        "text": "Autoimmune and chronic illness is a gaping black hole in medical science.",
+        "author": "Dr. Steven Philips, MD",
+        "date": "2020",
+        "description": "In his book, Chronic, Steven Philips, MD describes how doctors often diagnose people with autoimmune disorders when they are unable to find the true cause."
+    },
+    {
+        "text": "I had been labeled enough, and it nearly cost me my life.",
+        "author": "Dr. Steven Philips, MD",
+        "date": "2020",
+        "description": "When Steven Philips fell sick in 2010, the doctors labeled him with various disorders. Spondylitis, rheumatoid arthritis. The truth was, they missed a dangerous bacterial infection called Lyme disease."
+    },
+    {
+        "text": "I think you have middle-aged life crisis depression thing.",
+        "author": "Unknown psychotherapist",
         "date": null,
-        "description": null
-    }
+        "description": "Preston Wiles is a first-rank psychiatrist with training from Baylor, Harvard, and Yale. When he came down with Lyme (a bacterial infection prone to causing chronic illness), a psychotherapist told him this."
+    },
+    {
+        "text": "To cure something, isn't it important to know its cause and get to it, rather than mask its symptoms?",
+        "author": "Dr. Steven Philips, MD",
+        "date": "2020",
+        "description": "Autoimmune disorders are usually cured via expensive and lifelong immunosuppressants. But the truth is many of these chronic illnesses are caused by underlying bacterial infections like Lyme."
+    },
+    {
+        "text": "If it walks and quacks like a duck, it's probably either a species of duck or at least another water bird.",
+        "author": "Dr. Steven Philips, MD",
+        "date": "2020",
+        "description": "Steven Philips fell sick with a bacterial infection in 2010. He had symptoms of a Brucella infection, but he tested negative numerous times. However, it is likely he had a new species of Brucella, a species of Bartonella, or some other closely related bacterial infection."
+    },
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // {
+    //     "text": "I did not invite you over to my house for wine, just to throw your wife through my coffee table",
+    //     "author": "Sam Hyde",
+    //     "date": null,
+    //     "description": null
+    // }
 
     
 ]
